72def3b2114a1bba771129e275cc5fba4fbb18ef
djkazunoko <djkazunoko@gmail.com>
2024-07-19 15:53:21 +0900

    feat: 定員を超えて参加できないようにした

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,13 @@ class Ticket < ApplicationRecord
   belongs_to :group
 
   validates :group_id, uniqueness: { scope: :user_id }
+  validate :participants_cannot_exceed_capacity
+
+  private
+
+  def participants_cannot_exceed_capacity
+    return unless group.tickets.count >= group.capacity
+
+    errors.add(:base, '定員を超えて参加することはできません')
+  end
 end
