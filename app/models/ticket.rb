5c208711230117b97ba2e2589dfe175f436bd209
djkazunoko <djkazunoko@gmail.com>
2024-07-23 08:26:21 +0900

    feat: 自身が主催したグループには参加できないようにした (#76)

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,6 +6,7 @@ class Ticket < ApplicationRecord
 
   validates :group_id, uniqueness: { scope: :user_id }
   validate :participants_cannot_exceed_capacity
+  validate :owner_cannot_participate
 
   private
 
@@ -13,5 +14,11 @@ class Ticket < ApplicationRecord
     return unless group.tickets.count >= group.capacity
 
     errors.add(:base, '定員を超えて参加することはできません')
+  end
+
+  def owner_cannot_participate
+    return unless group.created_by?(user)
+
+    errors.add(:base, '自身が主催したグループには参加できません')
   end
 end
