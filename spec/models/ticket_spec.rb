6e5b102e2a7abc4af32a525b977df82e528bf206
djkazunoko <djkazunoko@gmail.com>
2024-08-28 15:34:34 +0900

    test: Ticketモデルのバリデーションのテストを追加 (#76)

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:ticket) }
+
+  it { is_expected.to validate_uniqueness_of(:group_id).scoped_to(:user_id) }
 end
