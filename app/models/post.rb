e2006dff3ef4b88c9b2b59a138bfc30a05ddd051
djkazunoko <djkazunoko@gmail.com>
2024-10-01 11:30:00 +0900

    feat: ログインしていれば誰でも投稿できるように変更した (#80)
    主催者・参加者でなければ投稿できない制約をなくした
    
    変更理由：参加していない状態でコメントしたいユーザもいるだろう

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,4 @@ class Post < ApplicationRecord
   belongs_to :group
 
   validates :content, presence: true
-  validate :user_cannot_post_if_not_member_or_owner
-
-  private
-
-  def user_cannot_post_if_not_member_or_owner
-    return if group.created_by?(user) || group.tickets.exists?(user:)
-
-    errors.add(:base, 'このグループに参加していないため、投稿できません')
-  end
 end
