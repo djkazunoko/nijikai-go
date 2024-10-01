a216be4d09e311c44eb41495cd1733f15c1dcf4b
djkazunoko <djkazunoko@gmail.com>
2024-10-01 11:30:00 +0900

    feat: ログインしていれば誰でも投稿できるように変更した
    主催者・参加者でなければ投稿できない制約をなくした
    
    変更理由：参加していない状態でコメントしたいユーザもいるだろう

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -7,42 +7,9 @@ RSpec.describe 'Posts', type: :request do
   let(:alice) { build(:user, :alice) }
 
   describe 'POST /groups/:id/posts' do
-    context 'when user is a member of the group' do
-      before do
-        create(:ticket, group:, user: alice)
-        github_mock(alice)
-        login
-      end
-
-      it 'creates a post and redirects to the group page' do
-        expect do
-          post group_posts_path(group), params: { post: { content: 'test post' } }
-        end.to change(Post, :count).by(1)
-
-        expect(response).to redirect_to(group_path(group))
-      end
-    end
-
-    context 'when user is not a member of the group' do
+    context 'when user is logged in' do
       before do
         github_mock(alice)
-        login
-      end
-
-      it 'does not create a post and redirects to the group page with an error message' do
-        expect do
-          post group_posts_path(group), params: { post: { content: 'test post' } }
-        end.not_to change(Post, :count)
-
-        expect(response).to redirect_to(group_path(group))
-        follow_redirect!
-        expect(response.body).to include('このグループに参加していないため、投稿できません')
-      end
-    end
-
-    context 'when user is the group owner' do
-      before do
-        github_mock(group.owner)
         login
       end
 
