d12adc8a62022315333ec46602fca0b8a24fdf0f
djkazunoko <djkazunoko@gmail.com>
2024-10-18 12:39:54 +0900

    style: 「投稿」に表記を統一した (#80)

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -13,14 +13,14 @@ RSpec.describe 'Posts', type: :system do
 
     it 'logs in and creates a post' do
       visit group_path(group)
-      expect(page).to have_content 'まだコメントはありません。'
+      expect(page).to have_content 'まだ投稿はありません。'
 
-      click_button 'サインアップ / ログインをしてコメントを投稿する'
+      click_button 'サインアップ / ログインをして投稿を作成する'
       expect(page).to have_current_path(group_path(group))
 
       expect do
         fill_in 'post_content', with: 'テストコメント'
-        click_button 'コメントを投稿する'
+        click_button '投稿を作成する'
         expect(page).to have_content '投稿が作成されました'
         expect(page).to have_content 'テストコメント'
         within('.chat-header') do
@@ -31,7 +31,7 @@ RSpec.describe 'Posts', type: :system do
           expect(page).to have_link(href: "https://github.com/#{alice.name}")
           expect(page).to have_css("img[src='#{alice.image_url}']")
         end
-        expect(page).not_to have_content 'まだコメントはありません。'
+        expect(page).not_to have_content 'まだ投稿はありません。'
       end.to change(Post, :count).by(1)
 
       expect(page).to have_current_path(group_path(group))
@@ -52,7 +52,7 @@ RSpec.describe 'Posts', type: :system do
           expect(page).not_to have_button '削除'
         end
 
-        click_button 'サインアップ / ログインをしてコメントを投稿する'
+        click_button 'サインアップ / ログインをして投稿を作成する'
 
         expect do
           accept_confirm do
@@ -79,7 +79,7 @@ RSpec.describe 'Posts', type: :system do
         visit group_path(group)
         expect(page).to have_content 'MyText'
 
-        click_button 'サインアップ / ログインをしてコメントを投稿する'
+        click_button 'サインアップ / ログインをして投稿を作成する'
 
         within('.chat') do
           expect(page).not_to have_button '削除'
