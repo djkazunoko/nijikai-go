a28a45c677be4c6a2c14182ba46a2ae20e29a318
djkazunoko <djkazunoko@gmail.com>
2025-01-13 11:00:15 +0900

    test: Group#created_by?メソッドのテストを作成 (#87)

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -15,4 +15,28 @@ RSpec.describe Group, type: :model do
     it { is_expected.to validate_numericality_of(:capacity).only_integer }
     it { is_expected.to validate_numericality_of(:capacity).is_greater_than(0) }
   end
+
+  describe '#created_by?' do
+    let(:owner) { create(:user) }
+    let(:other_user) { create(:user) }
+    let(:group) { create(:group, owner:) }
+
+    context 'when the user is the owner of the group' do
+      it 'returns true' do
+        expect(group.created_by?(owner)).to be true
+      end
+    end
+
+    context 'when the user is not the owner of the group' do
+      it 'returns false' do
+        expect(group.created_by?(other_user)).to be false
+      end
+    end
+
+    context 'when the user is nil' do
+      it 'returns false' do
+        expect(group.created_by?(nil)).to be false
+      end
+    end
+  end
 end
