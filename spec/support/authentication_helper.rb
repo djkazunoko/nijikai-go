85c3c4525082d4af60369a2fc73cd9431165a44e
djkazunoko <djkazunoko@gmail.com>
2025-01-15 22:19:17 +0900

    test: Ticketsのシステムスペックを修正 (#87)

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -3,6 +3,15 @@
 OmniAuth.config.test_mode = true
 
 module AuthenticationHelper
+  def login_as(user)
+    github_mock(user)
+    if RSpec.current_example.metadata[:type] == :request
+      get '/auth/github/callback'
+    else
+      visit '/auth/github/callback'
+    end
+  end
+
   def login
     get '/auth/github/callback'
   end
--- spec/system/tickets_spec.rb
+++ spec/system/tickets_spec.rb
@@ -3,133 +3,236 @@
 require 'rails_helper'
 
 RSpec.describe 'Tickets', type: :system do
-  let(:alice) { build(:user, :alice) }
-  let(:group) { create(:group) }
+  shared_context 'when the user is logged in' do
+    let(:user) { create(:user) }
 
-  describe 'group participation' do
-    context 'when not logged in' do
-      before do
-        github_mock(alice)
-      end
+    before do
+      login_as(user)
+    end
+  end
 
-      it 'logs in and allows the user to join the group' do
+  shared_context 'when the user is NOT logged in' do
+    let(:user) { create(:user) }
+
+    before do
+      github_mock(user)
+    end
+  end
+
+  shared_context 'when the user is the owner of the group' do
+    let(:group) { create(:group, owner: user) }
+  end
+
+  shared_context 'when the user is NOT the owner of the group' do
+    let(:group) { create(:group) }
+  end
+
+  shared_context 'when the user is a member of the group' do
+    before do
+      create(:ticket, user:, group:)
+    end
+  end
+
+  describe 'group participation by logged in user' do
+    include_context 'when the user is logged in'
+    context 'when the user is the owner of the group' do
+      include_context 'when the user is the owner of the group'
+
+      it 'does not display participation buttons and messages' do
         visit group_path(group)
 
-        expect(page).not_to have_css('.avatar')
-        expect(page).not_to have_link href: "https://github.com/#{alice.name}"
-        expect(page).to have_content "0 / #{group.capacity}人"
+        expect(page).not_to have_button 'この2次会グループに参加する'
+        expect(page).not_to have_button 'サインアップ / ログインをして2次会グループに参加'
         expect(page).not_to have_button '参加をキャンセルする'
-
-        expect do
-          click_button 'サインアップ / ログインをして2次会グループに参加'
-          expect(page).to have_content '2次会グループに参加しました'
-          expect(page).to have_css(".avatar img[src='#{alice.image_url}']")
-          expect(page).to have_link href: "https://github.com/#{alice.name}"
-          expect(page).to have_content "1 / #{group.capacity}人"
-          expect(page).to have_button '参加をキャンセルする'
-        end.to change(Ticket, :count).by(1)
+        expect(page).not_to have_content '定員に達したため参加できません'
       end
     end
 
-    context 'when logged in and meeting the participation requirements' do
+    context 'when the user is NOT the owner of the group AND is a member of the group AND the group is full' do
+      include_context 'when the user is NOT the owner of the group'
+      include_context 'when the user is a member of the group'
       before do
-        github_mock(alice)
-        visit '/auth/github/callback'
+        group.update!(capacity: 1)
       end
 
-      it 'allows the user to join the group' do
+      it 'displays a cancel button and allows cancellation' do
         visit group_path(group)
 
-        expect(page).not_to have_link href: "https://github.com/#{alice.name}"
-        expect(page).to have_content "0 / #{group.capacity}人"
+        expect(page).not_to have_button 'この2次会グループに参加する'
+        expect(page).not_to have_button 'サインアップ / ログインをして2次会グループに参加'
+        expect(page).not_to have_content '定員に達したため参加できません'
+
+        expect(page).to have_link href: "https://github.com/#{user.name}"
+        expect(page).to have_content "1 / #{group.capacity}人"
+
+        expect do
+          click_button '参加をキャンセルする'
+          expect(page).to have_content 'この2次会グループの参加をキャンセルしました'
+          expect(page).not_to have_link href: "https://github.com/#{user.name}"
+          expect(page).to have_content "0 / #{group.capacity}人"
+          expect(page).to have_button 'この2次会グループに参加する'
+        end.to change(Ticket, :count).by(-1)
+
+        expect(page).to have_current_path(group_path(group))
+      end
+    end
+
+    context 'when the user is NOT the owner of the group AND is a member of the group AND the group is NOT full' do
+      include_context 'when the user is NOT the owner of the group'
+      include_context 'when the user is a member of the group'
+
+      it 'displays a cancel button and allows cancellation' do
+        visit group_path(group)
+
+        expect(page).not_to have_button 'この2次会グループに参加する'
+        expect(page).not_to have_button 'サインアップ / ログインをして2次会グループに参加'
+        expect(page).not_to have_content '定員に達したため参加できません'
+
+        expect(page).to have_link href: "https://github.com/#{user.name}"
+        expect(page).to have_content "1 / #{group.capacity}人"
+
+        expect do
+          click_button '参加をキャンセルする'
+          expect(page).to have_content 'この2次会グループの参加をキャンセルしました'
+          expect(page).not_to have_link href: "https://github.com/#{user.name}"
+          expect(page).to have_content "0 / #{group.capacity}人"
+          expect(page).to have_button 'この2次会グループに参加する'
+        end.to change(Ticket, :count).by(-1)
+
+        expect(page).to have_current_path(group_path(group))
+      end
+    end
+
+    context 'when the user is NOT the owner of the group AND is not a member of the group AND the group is full' do
+      include_context 'when the user is NOT the owner of the group'
+      before do
+        create_list(:ticket, 10, group:)
+      end
+
+      it 'displays a full capacity message' do
+        visit group_path(group)
+
+        expect(page).to have_content '定員に達したため参加できません'
+
+        expect(page).not_to have_button 'この2次会グループに参加する'
         expect(page).not_to have_button 'サインアップ / ログインをして2次会グループに参加'
         expect(page).not_to have_button '参加をキャンセルする'
+      end
+    end
+
+    context 'when the user is NOT the owner of the group AND is not a member of the group AND the group is NOT full' do
+      include_context 'when the user is NOT the owner of the group'
+
+      it 'displays a participation button and allows participation' do
+        visit group_path(group)
+
+        expect(page).not_to have_button 'サインアップ / ログインをして2次会グループに参加'
+        expect(page).not_to have_button '参加をキャンセルする'
+        expect(page).not_to have_content '定員に達したため参加できません'
+
+        expect(page).not_to have_link href: "https://github.com/#{user.name}"
+        expect(page).to have_content "0 / #{group.capacity}人"
 
         expect do
           click_button 'この2次会グループに参加する'
           expect(page).to have_content '2次会グループに参加しました'
-          expect(page).to have_link href: "https://github.com/#{alice.name}"
+          expect(page).to have_link href: "https://github.com/#{user.name}"
           expect(page).to have_content "1 / #{group.capacity}人"
           expect(page).to have_button '参加をキャンセルする'
         end.to change(Ticket, :count).by(1)
+
+        expect(page).to have_current_path(group_path(group))
       end
     end
+  end
 
-    context 'when already participating in the group and not logged in' do
-      before do
-        github_mock(alice)
-        create(:ticket, user: alice, group:)
-      end
-
-      it 'does not allow the user to join the group' do
-        visit group_path(group)
-
-        expect(page).to have_link href: "https://github.com/#{alice.name}"
-
-        expect do
-          click_button 'サインアップ / ログインをして2次会グループに参加'
-          expect(page).to have_content 'この2次会グループには既に参加しています'
-        end.not_to change(Ticket, :count)
-      end
-    end
-
-    context 'when the group has reached its capacity' do
+  describe 'group participation by non logged in user' do
+    include_context 'when the user is NOT logged in'
+    context 'when the group is full' do
       let(:full_capacity_group) { create(:group, :full_capacity) }
 
-      it 'does not display participation button' do
+      it 'displays a full capacity message' do
         visit group_path(full_capacity_group)
-        expect(page).not_to have_button 'サインアップ / ログインをして2次会グループに参加'
+
         expect(page).to have_content '定員に達したため参加できません'
-      end
-    end
 
-    context 'when the user is the group owner and logged in' do
-      before do
-        github_mock(group.owner)
-        visit '/auth/github/callback'
-      end
-
-      it 'does not display participation button' do
-        visit group_path(group)
         expect(page).not_to have_button 'この2次会グループに参加する'
+        expect(page).not_to have_button 'サインアップ / ログインをして2次会グループに参加'
+        expect(page).not_to have_button '参加をキャンセルする'
       end
     end
 
-    context 'when the user is the group owner and not logged in' do
-      before do
-        github_mock(group.owner)
-      end
+    context 'when the user is the owner of the group AND is not a member of the group AND the group is NOT full' do
+      include_context 'when the user is the owner of the group'
 
-      it 'does not allow the user to join the group' do
+      it 'displays a log in and participation button but prevents participation for the owner' do
         visit group_path(group)
+
+        expect(page).not_to have_button 'この2次会グループに参加する'
+        expect(page).not_to have_button '参加をキャンセルする'
+        expect(page).not_to have_content '定員に達したため参加できません'
+
+        expect(page).not_to have_css('.avatar')
 
         expect do
           click_button 'サインアップ / ログインをして2次会グループに参加'
           expect(page).to have_content '自身が主催したグループには参加できません'
         end.not_to change(Ticket, :count)
+
+        expect(page).to have_css(".avatar img[src='#{user.image_url}']")
+        expect(page).to have_current_path(group_path(group))
       end
     end
-  end
 
-  describe 'group participation cancellation' do
-    before do
-      github_mock(alice)
+    context 'when the user is NOT the owner of the group AND is a member of the group AND the group is NOT full' do
+      include_context 'when the user is NOT the owner of the group'
+      include_context 'when the user is a member of the group'
+
+      it 'displays a log in and participation button but prevents participation for members' do
+        visit group_path(group)
+
+        expect(page).not_to have_button 'この2次会グループに参加する'
+        expect(page).not_to have_button '参加をキャンセルする'
+        expect(page).not_to have_content '定員に達したため参加できません'
+
+        expect(page).not_to have_css('.avatar')
+
+        expect do
+          click_button 'サインアップ / ログインをして2次会グループに参加'
+          expect(page).to have_content 'この2次会グループには既に参加しています'
+        end.not_to change(Ticket, :count)
+
+        expect(page).to have_css(".avatar img[src='#{user.image_url}']")
+        expect(page).to have_current_path(group_path(group))
+      end
     end
 
-    it 'allows the user to cancel participation' do
-      visit group_path(group)
-      click_button 'サインアップ / ログインをして2次会グループに参加'
+    context 'when the user is NOT the owner of the group AND is not a member of the group AND the group is NOT full' do
+      include_context 'when the user is NOT the owner of the group'
 
-      expect(page).to have_link href: "https://github.com/#{alice.name}"
-      expect(page).to have_content "1 / #{group.capacity}人"
+      it 'displays a log in and participation button and allows participation' do
+        visit group_path(group)
 
-      expect do
-        click_button '参加をキャンセルする'
-        expect(page).to have_content 'この2次会グループの参加をキャンセルしました'
-        expect(page).not_to have_link href: "https://github.com/#{alice.name}"
+        expect(page).not_to have_button 'この2次会グループに参加する'
+        expect(page).not_to have_button '参加をキャンセルする'
+        expect(page).not_to have_content '定員に達したため参加できません'
+
+        expect(page).not_to have_css('.avatar')
+
+        expect(page).not_to have_link href: "https://github.com/#{user.name}"
         expect(page).to have_content "0 / #{group.capacity}人"
-        expect(page).to have_button 'この2次会グループに参加する'
-      end.to change(Ticket, :count).by(-1)
+
+        expect do
+          click_button 'サインアップ / ログインをして2次会グループに参加'
+          expect(page).to have_content '2次会グループに参加しました'
+          expect(page).to have_link href: "https://github.com/#{user.name}"
+          expect(page).to have_content "1 / #{group.capacity}人"
+          expect(page).to have_button '参加をキャンセルする'
+        end.to change(Ticket, :count).by(1)
+
+        expect(page).to have_css(".avatar img[src='#{user.image_url}']")
+        expect(page).to have_current_path(group_path(group))
+      end
     end
   end
 end
