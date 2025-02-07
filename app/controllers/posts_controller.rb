53c9912fa240befd59d6bd69843efe2f72a31700
djkazunoko <djkazunoko@gmail.com>
2025-02-07 21:39:46 +0900

    feat: postの作成をTurbo Stremas化 (#82)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,26 @@
 
 class PostsController < ApplicationController
   def create
-    group = Group.find(params[:group_id])
-    post = current_user.posts.build do |t|
-      t.group = group
+    @group = Group.find(params[:group_id])
+    @post = current_user.posts.build do |t|
+      t.group = @group
       t.content = params[:post][:content]
     end
 
-    if post.save
-      redirect_to group, notice: '投稿が作成されました'
-    else
-      redirect_to group, alert: post.errors.full_messages.to_sentence
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @group, notice: '投稿が作成されました' }
+        format.turbo_stream { flash.now[:notice] = '投稿が作成されました' }
+      else
+        format.html { redirect_to @group, alert: @post.errors.full_messages.to_sentence }
+        format.turbo_stream do
+          flash.now[:alert] = @post.errors.full_messages.to_sentence
+          render turbo_stream: [
+            turbo_stream.update('new_post', partial: 'posts/form', locals: { group: @group }),
+            turbo_stream.prepend('flash', partial: 'application/flash')
+          ]
+        end
+      end
     end
   end
 
