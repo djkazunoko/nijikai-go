7654cf000896d8ab42bcbfd87f521cc89db1305e
djkazunoko <djkazunoko@gmail.com>
2025-02-11 23:13:28 +0900

    feat: Postsテーブルのuser_idカラムにNOT NULL制約を追加 (#100)
    rails g migration ChangeUserIdNullConstraintOnPostsを実行 → マイグレーションファイルを修正 → rails db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_20_005845) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_11_140722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_20_005845) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.bigint "group_id", null: false
     t.text "content", null: false
     t.datetime "created_at", null: false
