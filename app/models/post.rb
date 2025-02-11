9163039af33957b0c0363065126617ccaabf49a2
djkazunoko <djkazunoko@gmail.com>
2025-02-11 23:18:06 +0900

    feat: optional: trueオプションを削除 (#100)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   belongs_to :group
 
   validates :content, length: { maximum: 2000 }, presence: true
