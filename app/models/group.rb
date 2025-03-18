e06fc66e917439e1ad4629173647e98a3924fe9a
djkazunoko <djkazunoko@gmail.com>
2025-03-18 14:53:59 +0900

    feat: Twitterのハッシュタグで利用できる文字のみに制限するバリデーションを追加 (#99)
    スペースや記号を使用できないようにするformatバリデーションを追加
    
    数字のみ、アンダースコアのみ、数字とアンダースコアのみでは保存できないようにするカスタムバリデーションを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,13 +5,14 @@ class Group < ApplicationRecord
   has_many :tickets, dependent: :destroy
   has_many :posts, dependent: :destroy
 
-  validates :hashtag, presence: true, length: { maximum: 50 }
+  validates :hashtag, presence: true, length: { maximum: 50 }, format: { with: /\A[\p{Alnum}_]+\z/, message: 'にはスペースや記号は使用できません。' }
   validates :details, presence: true, length: { maximum: 2000 }
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :location, presence: true, length: { maximum: 100 }
   validates :payment_method, presence: true, length: { maximum: 50 }
 
   validate :capacity_cannot_be_less_than_participants, on: :update
+  validate :hashtag_cannot_be_numbers_or_underscores_only
 
   def created_by?(user)
     return false unless user
@@ -33,5 +34,12 @@ class Group < ApplicationRecord
     return unless capacity < tickets.count
 
     errors.add(:capacity, 'は参加人数以上の値にしてください')
+  end
+
+  def hashtag_cannot_be_numbers_or_underscores_only
+    return if hashtag.blank?
+    return unless hashtag.match?(/\A[0-9_]+\z/)
+
+    errors.add(:hashtag, 'は数字、アンダースコアのみでは登録できません')
   end
 end
