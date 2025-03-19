67eaaf1be49a3aa353584617b51607d6408190d8
djkazunoko <djkazunoko@gmail.com>
2025-03-19 18:51:00 +0900

    chore: production環境へのデプロイ時のアセットのプリコンパイルが通るようにした (#135)
    https://fly.io/docs/rails/getting-started/existing/#access-to-environment-variables-at-build-time

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github,
-    Rails.application.credentials.github[:client_id],
-    Rails.application.credentials.github[:client_secret]
+  if Rails.application.credentials.github
+    provider :github,
+      Rails.application.credentials.github[:client_id],
+      Rails.application.credentials.github[:client_secret]
+  end
 
   OmniAuth.config.on_failure = Proc.new { |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
