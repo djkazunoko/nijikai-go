358a46f76e3daaec5a2a99bcebcea40854d570af
djkazunoko <djkazunoko@gmail.com>
2025-04-03 15:24:51 +0900

    feat: OGP画像を設定 (#120)

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -7,7 +7,30 @@ module MetaTagsHelper
       reverse: true,
       charset: 'utf-8',
       description: 'テックイベントの2次会参加者募集ツール',
-      viewport: 'width=device-width, initial-scale=1.0'
+      viewport: 'width=device-width, initial-scale=1.0',
+      og:,
+      twitter:
+    }
+  end
+
+  private
+
+  def og
+    {
+      site_name: :site,
+      title: :title,
+      description: :description,
+      type: 'website',
+      url: 'https://nijikai-go.fly.dev',
+      local: 'ja-JP',
+      image: image_url('ogp.png')
+    }
+  end
+
+  def twitter
+    {
+      card: 'summary',
+      image: image_url('ogp.png')
     }
   end
 end
