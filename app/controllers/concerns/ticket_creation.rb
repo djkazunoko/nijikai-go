003f018d13b6fe65bb7adedfa9143178bac7ff27
djkazunoko <djkazunoko@gmail.com>
2025-04-09 15:20:03 +0900

    feat: saveまでをlockしたtransaction内で行うためにwith_lockをmodel側からcontroller側に移動 (#148)

--- app/controllers/concerns/ticket_creation.rb
+++ app/controllers/concerns/ticket_creation.rb
@@ -7,11 +7,14 @@ module TicketCreation
 
   def create_ticket_and_redirect(user, group_id)
     group = Group.find(group_id)
-    ticket = user.tickets.build(group:)
-    if ticket.save
-      redirect_to group, notice: '2次会グループに参加しました。'
-    else
-      redirect_to group, alert: ticket.errors.full_messages.to_sentence
+
+    group.with_lock do
+      ticket = user.tickets.build(group:)
+      if ticket.save
+        redirect_to group, notice: '2次会グループに参加しました。'
+      else
+        redirect_to group, alert: ticket.errors.full_messages.to_sentence
+      end
     end
   end
 end
